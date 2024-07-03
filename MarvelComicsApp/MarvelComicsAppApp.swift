e85d72b5027d65ac638d289a29c4dcd4b4395a2e
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-07-03 11:55:07 +0200

    first commit

---
+++ MarvelComicsApp/MarvelComicsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsAppApp.swift
+//  MarvelComicsApp
+//
+//  Created by Влад on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
